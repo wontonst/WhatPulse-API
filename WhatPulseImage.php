ebdec444157d9ba63f57410e2dc5e87cd2dafadf
YiWei Zheng <yiweizhe@usc.edu>
2012-12-07 11:37:26 -0800

    testing img

--- WhatPulseImage.php
+++ WhatPulseImage.php
@@ -1,27 +1,20 @@
 <?
 
+define('BG','bg/');
+define('FONT','fonts/');
+
 function create_image() 
 { 
-	$image = imagecreatefrompng('bg/tree.png');  
+	$image = imagecreatefrompng(BG.'tree.png');  
 
     //We are making three colors, white, black and gray 
     $white = ImageColorAllocate($image, 255, 255, 255); 
     $black = ImageColorAllocate($image, 0, 0, 0); 
     $grey = ImageColorAllocate($image, 204, 204, 204); 
 
-    //Make the background black 
-    ImageFill($image, 0, 0, $black); 
+//adding username
+    imagettftext($image, 20, 0.0, 0,0,$white,FONT.'adventure_subtitles.ttf','wontonst' ); 
 
-    //Add randomly generated string in white to the image
-    ImageString($image, 3, 30, 3, $pass, $white); 
-
-    //Throw in some lines to make it a little bit harder for any bots to break 
-    ImageRectangle($image,0,0,$width-1,$height-1,$grey); 
-    imageline($image, 0, $height/2, $width, $height/2, $grey); 
-    imageline($image, $width/2, 0, $width/2, $height, $grey); 
- 
-    //Tell the browser what kind of file is come in 
-    header("Content-Type: image/jpeg"); 
 
     //Output the newly created image in jpeg format 
     ImageJpeg($image,'out.jpg'); 
