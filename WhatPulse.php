fd72fc8a111d598345df4a2c5fb630865e728e48
YiWei Zheng <yiweizhe@usc.edu>
2012-12-23 00:30:29 -0800

    overhauling

--- WhatPulse.php
+++ WhatPulse.php
@@ -1,4 +1,29 @@
 <?
+class Stat{
+private $data;///<contains total perminute perhour perday
+function __construct($tot,&$totaltime,$precision=2)
+{
+$this->data['total'] = $tot;
+$this->calculate($totaltime);
+$this->format($precision);
+}
+function __get($v)
+{
+return $this->data[$v];
+}
+private function calculate(&$time)
+{
+$this->data['perminute'] = $this->total/($time/60);
+$this->data['perhour'] = $this->total/($time/3600);
+$this->data['perday'] = $this->total/($time/86400);
+}
+private function format(&$precision)
+{
+array_walk($this->data,function(&$value,$key) use ($precision){
+$value = number_format($value,$precision);
+});
+}
+}
 /**
 An alternative to whatever the WhatPulse website script offers. To be honest this was written before I discovered that WhatPulse has their own script ready to be deployed. Nonetheless, this code does appear to work and should suffice for most purposes.
 @brief retrieves data from the whatpulse web API
@@ -20,6 +45,11 @@ class WhatPulse {
     private $minutes;///<user account age in minutes(string formatted)
     private $hours;///<user account age in hours(string formatted)
     private $days;///<user account age in days (string formatted)
+private $network;///<user total network operations in megabytes (string formatted)
+private $networkratio;///<download:upload ratio
+private $download;///<user download in megabytes (string formatted)
+private $upload;///<user upload in megabytes (string formatted)
+private $uptime;///<user total uptime in hours(string formatted)
     private $lastpulse;///<unix timestamp of last pulse
     private $lastpulseago;///<seconds between now and last pulse
     private $_retrievable = array('id','totalclicks','totalkeys','kperminute','cperminute','kperhour','cperhour','kperday','cperday','minutes','hours','days');///<variables retrievable using magic functions
@@ -71,7 +101,7 @@ class WhatPulse {
         $cperminute = $totalclicks/$minutes;
         $cperhour = $totalclicks/$hours;
         $cperday = $totalclicks/$days;
-//formatting
+//click/keypress formatting
         $this->totalclicks = number_format($totalclicks,0);
         $this->totalkeys = number_format($totalkeys,0);
         $this->kperminute = number_format($kperminute,2);
@@ -84,6 +114,7 @@ class WhatPulse {
         $this->hours = number_format($hours,2);
         $this->days = number_format($days,2);
 
+//lastpulse
         $temp = date_default_timezone_get();//temporarily store current timezone
         date_default_timezone_set('Europe/Belgrade');//belgrade is where server located
         $datetime = new DateTime($this->xml->LastPulse);//create new DT from belgrade time
@@ -93,14 +124,20 @@ class WhatPulse {
 
         $this->lastpulse = $datetime->getTimestamp();//set lastpulse unix timestamp
         $this->lastpulseago = time()-$this->lastpulse;//get time diff between now and lastpulse
-
-
 //echo $datetime->format('Y-m-d H:i:s').'::::'.$this->xml->LastPulse;
+
+//network
+$this->network = number_format($this->xml->DownloadMB+$this->xml->UploadMB,2);
+$this->networkratio = number_format($this->xml->DownloadMB/$this->xml->UploadMB/8,2);
+$this->download = new Stat($this->xml->DownloadMB+0.0,$totaltime);
+$this->upload = new Stat($this->xml->UploadMB+0.0,$totaltime);
+
+$this->uptime = number_format($this->xml->UptimeSeconds/3600,2);
     }
     /**
     @brief grabs the data from the WhatPulse API, setting the object's SimpleXMLElement
     */
-    private    function getXML() {
+    private function getXML() {
         $url = 'http://whatpulse.org/api/user.php?format=xml&user=';
 
         $f = fopen($url.$this->id,'r');
@@ -110,6 +147,7 @@ class WhatPulse {
         $content = stream_get_contents($f);
 //echo $content;
         $this->xml = new SimpleXMLElement($content);
+//var_dump($this->xml);
     }
     /**
     @brief useful for debugging, prints out all class data
@@ -121,8 +159,14 @@ class WhatPulse {
         echo "\t".$this->kperminute.'/minute'."\n\t".$this->kperhour.'/hour'."\n\t".$this->kperday.'/day'."\n";
         echo 'Mouse clicks: '.$this->totalclicks."\n";
         echo "\t".$this->cperminute.'/minute'."\n\t".$this->cperhour.'/hour'."\n\t".$this->cperday.'/day'."\n";
-        echo 'Total miles: '.$this->xml->TotalMiles."\n";
+echo 'Total network operations: '.$this->network.' MBytes ('.$this->networkratio.' D/U ratio'.")\n";
+echo "\t".$this->download->total.' MBytes downloaded'."\n";
+echo "\t\t".$this->download->perminute.'/minute'."\n";
+echo "\t\t".$this->download->perhour.'/hour'."\n";
+echo "\t\t".$this->download->perday.'/day'."\n";
+echo "\t".$this->upload->total.' MBytes uploaded'."\n";
         echo 'Date joined: '.$this->xml->DateJoined.' ('.$this->days.' days)'."\n";
+
     }
 
 }
--- sample.php
+++ sample.php
@@ -7,7 +7,7 @@ $stats->printStats();
 
 //$stats = new WhatPulse(1321);//invalid ID
 
-$web = new WhatPulseWeb($stats);
-echo $web->make();
+//$web = new WhatPulseWeb($stats);
+//echo $web->make();
 
 ?>
